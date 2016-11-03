385915b9c50d4d86f8d8947dc944e4d3bc5aca3d
Losko <lozan_vlashev@abv.bg>
2016-11-03 23:29:20 +0200

    Added file in server > config > express.js edited index.js

--- index.js
+++ index.js
@@ -7,20 +7,15 @@ let env = process.env.NODE_ENV || 'development'
 let config = require('./server/config/config')[env]
 
 require('./server/config/database')(config)
+require('./server/config/express')(config, app)
 
 let port = process.env.port || 80
-
-
-app.set('view engine', 'pug')
-app.set('views', './server/views')
 
 app.get('/', (req, res) => {
     "use strict";
     mongoose.connect(config.db)
     res.render('index')
 })
-
-app.use(express.static('public'))
 
 app.listen(config.port)
 
