544fea469ca79d71d1e531bc4ca9fa2a9308287b
Losko <lozan_vlashev@abv.bg>
2016-11-03 22:37:48 +0200

    Added folders public, server, views and css. Set environment. File added: site.css, index.pug

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+mongoose.Promise = global.Promise
+
 let app = express()
+
+let env = process.env.NODE_ENV || 'development'
+let port = process.env.port || 80
+
+app.set('view engine', 'pug')
+app.set('views', './server/views')
 
 app.get('/', (req, res) => {
     "use strict";
     mongoose.connect('mongodb://localhost:27017/codeNameSite')
-    console.log('MongoDB Up and Rdy!')
+    console.log('MongoDB Up and RDY!')
 
-    res.send('Cool')
+    res.render('index')
 })
 
-app.listen(80)
+app.use(express.static('public'))
+
+app.listen(port)
 
 console.log('express RDY!')
 
