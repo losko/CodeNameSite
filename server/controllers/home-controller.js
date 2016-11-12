f4da46f1066250b30fc2d4889689f80ba45beb68
Losko <lozan_vlashev@abv.bg>
2016-11-12 20:22:49 +0200

    Added basic functionality for displaying content.

--- server/controllers/home-controller.js
+++ server/controllers/home-controller.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose')
+const Literature = mongoose.model('Literature')
+
 module.exports = {
     index: (req, res) => {
         "use strict";
-        res.render('home/index')
+        Literature.find({}).limit(6).populate('author')
+            .then(literatures => {
+                res.render('home/index', {literatures: literatures})
+            })
     },
     about: (req, res) => {
         "use strict";
